75af0327a75632ad890e642cd2f1c392d92c52b4
Rinat <rinat.luhmanov@mail.ru>
2020-03-16 22:18:09 +0700

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,6 +7,7 @@ const Dialogs = (props) => {
 
     let state = props.dialogsPage;
 
+
     let dialogsElements = state.dialogs.map( (d) => <DialogItem name={d.name} id={d.id}/>); 
     let messagesElements = state.messages.map( m  => <Message message={m.message} id={m.id}/>);
     let newMessageBody = state.newMessageBody;
